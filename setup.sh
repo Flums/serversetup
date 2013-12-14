962eaf402b7c0fefc4e6188cd9f9668f0a50114d
Philip Gabrielsen <philip@digno.no>
2013-12-14 03:17:31 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 yum clean all
+yum update -y
 yum install vim -y
 cd /etc/ssh
 wget -N https://raw.github.com/Flums/serversetup/master/sshd_config
