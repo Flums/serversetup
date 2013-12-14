b9dd547656cfc637ff1cd9f10dbb8c85763afa6a
Philip Gabrielsen <philip@digno.no>
2013-12-14 04:35:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+cp -f /usr/share/zoneinfo/Europe/Oslo /etc/localtime
 rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 yum clean all
