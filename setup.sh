cbc193dc59472b873f66a2fd667bc8b9c4c6a202
Philip Gabrielsen <philip@digno.no>
2013-12-14 03:11:28 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+yum clean all
+yum install vim -y
+cd /etc/ssh
+wget -N 
