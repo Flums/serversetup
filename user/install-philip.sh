137682c0e3f6d673ca386c7b4c207fd74b914007
Philip Gabrielsen <philip@redpill-linpro.com>
2016-05-11 15:08:07 +0200

    Removing multiple curl

--- user/install-philip.sh
+++ user/install-philip.sh
@@ -1,4 +1,4 @@
 useradd philip
 mkdir /home/philip/.ssh
-curl curl https://raw.githubusercontent.com/Flums/serversetup/master/user/philip-pubkey > /home/philip/.ssh/authorized_keys
+curl https://raw.githubusercontent.com/Flums/serversetup/master/user/philip-pubkey > /home/philip/.ssh/authorized_keys
 echo "philip   ALL = NOPASSWD: ALL" >> /etc/sudoers
