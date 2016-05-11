b6f0924c64b5027e19a411be7fc02368adf8a67e
Philip Gabrielsen <philip@redpill-linpro.com>
2016-05-11 15:15:35 +0200

    useradd => adduser

--- user/install-philip.sh
+++ user/install-philip.sh
@@ -1,4 +1,4 @@
-useradd philip
+adduser --disabled-password --gecos "" philip
 mkdir /home/philip/.ssh
 curl https://raw.githubusercontent.com/Flums/serversetup/master/user/philip-pubkey > /home/philip/.ssh/authorized_keys
 echo "philip   ALL = NOPASSWD: ALL" >> /etc/sudoers
