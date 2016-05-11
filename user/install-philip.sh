4ae3ade56fd28b16f1771eb21cbecfa7ff8a5260
Philip Gabrielsen <philip@redpill-linpro.com>
2016-05-11 15:02:34 +0200

    Sudoers

--- user/install-philip.sh
+++ user/install-philip.sh
@@ -1,3 +1,4 @@
 useradd philip
 mkdir /home/philip/.ssh
-curl 
+curl curl https://raw.githubusercontent.com/Flums/serversetup/master/user/philip-pubkey > /home/philip/.ssh/authorized_keys
+echo "philip   ALL = NOPASSWD: ALL" >> /etc/sudoers
