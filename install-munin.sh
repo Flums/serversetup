e3403db904bd4cf7a7c9013f184303a0d6809abd
Philip Gabrielsen <philip@redpill-linpro.com>
2016-05-11 16:04:52 +0200

    asd

--- install-munin.sh
+++ install-munin.sh
@@ -1,2 +1,8 @@
-apt-get install munin -y
+apt-get install -y apache2 apache2-utils
+apt-get install -y libcgi-fast-perl libapache2-mod-fcgid
+apt-get install -y munin
 curl https://raw.githubusercontent.com/Flums/serversetup/master/munin/munin.conf > /etc/munin/munin.conf
+mkdir /var/www/munin
+chown munin:munin /var/www/munin
+ln -s /etc/munin/apache.conf /etc/apache2/sites-enabled/
+service apache2 restart
