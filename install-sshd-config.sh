5317ca01dde2cfabccfa7e3ac02924182901229c
Philip Gabrielsen <philip@redpill-linpro.com>
2016-05-12 11:48:01 +0200

    Ssh restart added

--- install-sshd-config.sh
+++ install-sshd-config.sh
@@ -1,3 +1,4 @@
 sed -i 's/Port 22/Port 39029/g' /etc/ssh/sshd_config
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 apt-get -y install fail2ban
+service ssh restart
