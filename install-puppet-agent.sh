5e4653f53d6215c2873f64fde95178368d20ceeb
Philip Gabrielsen <philip@redpill-linpro.com>
2018-01-20 00:16:04 +0100

    Install the puppet client

---
+++ install-puppet-agent.sh
@@ -0,0 +1,6 @@
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+sudo dpkg -i puppetlabs-release-pc1-xenial.deb
+sudo apt-get update
+sudo apt-get install puppet-agent
+sudo systemctl enable puppet
+sudo systemctl start puppet
